edc14863ea0ac0a14ee3b73a12269d20852d04e7
@RobertT-Ab4 <rober.tataru@ab4.systems>
2020-03-23 13:59:49 +0200

    Lab5

---
+++ Laborator5/Model/ModelSelfReference.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace EF_StudiiDeCaz.Model
+{
+    class ModelSelfReferences : DbContext
+    {
+        public ModelSelfReferences()
+            : base("name=ModelSelfReferences")
+        {
+        }
+        public virtual DbSet<SelfReference> SelfReferences { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<SelfReference>()
+                .HasMany(m => m.References)
+                .WithOptional(m => m.ParentSelfReference);
+
+        }
+    }
+}
